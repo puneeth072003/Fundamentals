1e88752fb0eb51ccd0950aa2f3abb980ba2a1931
Tejaswa <tejaswad18@gmail.com>
2024-07-10 12:47:13 +0530

    adding the feching method

--- Backend/src/router/route.js
+++ Backend/src/router/route.js
@@ -2,9 +2,11 @@ const express = require("express");
 const getHome =require("../controller/tasks.js");
 const { login, signin, getCurrentUser } = require("../controller/db.js");
 const bcrypt = require('bcrypt');
-const User = require('../controller/UserModel.js')
-
+const User = require('../controller/models/UserModel.js')
 const router = express.Router();
+const { MongoClient, ObjectId } = require('mongodb');
+const {fetchClass11Element} = require("../controller/tasks.js")
+const Class11 = require('../controller/models/ClassModel.js'); 
 
 router.get("/", getHome);
 router.post('/login', async (req, res) => {
@@ -33,7 +35,21 @@ router.post('/signin', async (req, res) => {
       res.status(400).send({ message: 'Error registering user' });
     }
   });
+
+  router.get('/class/:className', async (req, res) => {
+    try {
+      const className = req.params.className;
+      const class11Element = await Class11.findOne({ title1: className }, 'class11');
   
+      if (class11Element) {
+        res.json(class11Element);
+      } else {
+        res.status(404).send('Class not found');
+      }
+    } catch (error) {
+      res.status(500).send('Error fetching class11 element');
+    }
+  });
 router.get('/getuser',getCurrentUser);
 
 module.exports = router;
