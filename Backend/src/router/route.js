0fd72d37b6b029b74f5b0236cbc05d7e6c6261d2
Puneeth Y <pyd773@gmail.com>
2024-07-10 10:13:11 +0530

    adding the rouutes

--- Backend/src/router/route.js
+++ Backend/src/router/route.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const getHome =require("../controller/tasks.js");
+const { login, signin, getCurrentUser } = require("../controller/db.js");
 
 const router = express.Router();
 
 router.get("/", getHome);
+router.get("/login",login);
+router.get("/signin",signin);
+router.get('/getuser',getCurrentUser);
 
 module.exports = router;
